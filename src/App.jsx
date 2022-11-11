79d16f065d721464427cb543f2ce2cfb67eab036
Anton Shlain <ash.glc@gmail.com>
2022-11-11 15:18:46 +0600

    before Clarifai

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-// import './App.css';
+import './App.css';
+import ParticlesBg from 'particles-bg';
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
 import Navigation from "./components/Navigation/Navigation";
+import Rank from './components/Rank/Rank';
 
 function App() {
   return (
     <div>
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
+      <ParticlesBg type="cobweb" bg={true} />
     </div>
   );
 }
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,9 @@
+const Rank = () => {
+  return (
+    <div className="white f3 center">
+      {'Ash, your current rank is #5'}
+    </div>
+  )
+}
+
+export default Rank;
